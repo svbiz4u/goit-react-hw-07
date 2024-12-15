e769d795ec0b398bdc72b2205b789fe9aba42968
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-12-15 16:16:16 +0100

    Add files via upload

--- eslint.config.js
+++ eslint.config.js
@@ -11,26 +11,26 @@ export default [
   languageOptions: {
   ecmaVersion: 2020,
   globals: globals.browser,
-  parserOptions: {
+   parserOptions: {
   ecmaVersion: "latest",
   ecmaFeatures: { jsx: true },
-  sourceType: "module",
+   sourceType: "module",
   },
   },
   settings: { react: { version: "18.3" } },
   plugins: {
   react,
   "react-hooks": reactHooks,
-  "react-refresh": reactRefresh,
+   "react-refresh": reactRefresh,
   },
   rules: {
   ...js.configs.recommended.rules,
   ...react.configs.recommended.rules,
   ...react.configs["jsx-runtime"].rules,
-   ...reactHooks.configs.recommended.rules,
-  "react/prop-types": 0,
+  ...reactHooks.configs.recommended.rules,
+   "react/prop-types": 0,
   "react/jsx-no-target-blank": "off",
-  "react-refresh/only-export-components": [
+   "react-refresh/only-export-components": [
   "warn",
   { allowConstantExport: true },
   ],
